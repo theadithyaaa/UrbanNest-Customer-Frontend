ef0efac70acb5767710254e5185b8f6f541be3ae
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-15 13:49:14 +0530

    login fixed

--- src/app/pages/hero-page/hero-page.component.ts
+++ src/app/pages/hero-page/hero-page.component.ts
@@ -1,26 +1,24 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-hero-page',
   standalone: true,
-  imports: [ RouterLink, FormsModule, CommonModule, ],
+  imports: [ RouterLink, FormsModule, CommonModule, NgFor ],
   templateUrl: './hero-page.component.html',
   styleUrl: './hero-page.component.css'
 })
-export class HeroPageComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+
+export class HeroPageComponent {
   public login: any = {
     name: "",
     email: "",
-    password: ""
-    
+    password:"" 
   }
-  async createaccount() {
+  
+  async logincreate() {
     let response = await fetch("http://localhost:8080/login/add-login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -28,18 +26,28 @@ export class HeroPageComponent implements OnInit {
         this.login = {
           "name": this.login.name,
           "email": this.login.email,
-          "password": this.login.password
+          "password": this.login.password,
           
         }
       )
     })
-    alert('accout created successfully');
+    alert('login created successfully');
+    window.location.href = "http://localhost:4200/dashboard";
     let body = await response.json()
     alert(JSON.stringify(body));
-    window.location.href = "/dashboard";
     return body;
     
-    
   }
+
+  // clearFields() {
+  //   this.property = {
+  //     owner: "",
+  //     ownercontact: "",
+  //     location: "",
+  //     district: "",
+  //     price: "",
+  //     type: ""
+  //   };
+  // }
 
 }
