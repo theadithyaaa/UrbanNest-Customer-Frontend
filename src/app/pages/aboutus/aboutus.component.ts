6862f71012981f4a24c232cfa3e2c48be6a253c1
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-16 22:51:49 +0530

    about us

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from "../../common/navbar/navbar.component";
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.css'
 })
--- src/app/pages/agent/agent.component.ts
+++ src/app/pages/agent/agent.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from "../../common/navbar/navbar.component";
 
 @Component({
   selector: 'app-agent',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './agent.component.html',
   styleUrl: './agent.component.css'
 })
