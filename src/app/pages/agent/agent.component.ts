c522b6590827635bdd0f5dd75c2c53b9104d4126
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-22 23:43:51 +0530

    final commit

--- src/app/pages/agent/agent.component.ts
+++ src/app/pages/agent/agent.component.ts
@@ -2,14 +2,44 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from "../../common/navbar/navbar.component";
 import { FooterComponent } from "../../common/footer/footer.component";
+import emailjs from "emailjs-com";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agent',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FooterComponent],
+  imports: [RouterLink, NavbarComponent, FooterComponent, FormsModule],
   templateUrl: './agent.component.html',
-  styleUrl: './agent.component.css'
+  styleUrls: ['./agent.component.css']
 })
 export class AgentComponent {
 
+  async sendEmail(contactForm: any): Promise<void> {
+    try {
+      const { contactName, subject, message } = contactForm.value; 
+      const templateParams = {
+        from_name: contactName,
+        subject: subject,
+        message: message,
+        to_name: "UrbanNest Team",  
+        to_email: "urbannest.test@gmail.com", 
+      };
+
+      const response = await emailjs.send(
+        'service_mwjekw9', 
+        'template_p5uy7me',  
+        templateParams,
+        '4aWBEX6hIozmLixP3'  
+      );
+
+      if (response.status === 200) {
+        alert('Message sent successfully!');
+      } else {
+        alert('Failed to send message. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('An error occurred while sending the email. Please try again.');
+    }
+  }
 }
